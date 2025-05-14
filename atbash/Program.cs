7e413ae642684fbe26d28be1118ec51dc41082c2
baruch <baruchminzberg@gmail.com>
2025-05-14 17:49:11 +0300

    dangerousWordsFuncshion

--- atbash/Program.cs
+++ atbash/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,27 @@ namespace atbash
 {
     internal class Program
     {
+
+        static string dangerousWords(string message)
+        {
+            int count = 0;  
+            string[] messageList = message.Split(' '); // Split the message into words
+            string[] dangerousWords = { "bomb", "nukhba", "fighter", "rocket", "secret" }; // List of dangerous words
+            foreach (string word in messageList)
+            {
+                foreach (string dangerous in dangerousWords)
+                {
+                    if(word == dangerous)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return message+ "       count of dangerousWords: " +count;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(  dangerousWords("jhtkvyb bomb kjhg bomb"));
         }
     }
 }
