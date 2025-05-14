45903e95284ac750c531d4bfc6e556f5d6b307b5
baruch <baruchminzberg@gmail.com>
2025-05-14 18:24:32 +0300

    riskLevelFuncshion

--- atbash/Program.cs
+++ atbash/Program.cs
@@ -9,11 +9,11 @@ namespace atbash
 {
     internal class Program
     {
-
+        //Receives translated ciphertext, counts the number of dangerous words, returns both.   
         static string dangerousWords(string message)
         {
             int count = 0;  
-            string[] messageList = message.Split(' '); // Split the message into words
+            string[] messageList = message.Split(' '); 
             string[] dangerousWords = { "bomb", "nukhba", "fighter", "rocket", "secret" }; // List of dangerous words
             foreach (string word in messageList)
             {
@@ -25,11 +25,48 @@ namespace atbash
                     }
                 }
             }
-            return message+ "       count of dangerousWords: " +count;
+            return message+ "       count of dangerous words: " +count;
         }
+
+        //Writes a message according to the risk level and prints everything
+        static void riskLevel(string message, int sumWords)
+        {
+            string levelOfRisk;
+
+            if (sumWords > 0 && sumWords < 6)
+            {
+                levelOfRisk = "WARNING";
+            }
+            else if (sumWords >= 6 && sumWords < 11)
+            {
+                levelOfRisk = "DANGER!";
+            }
+            else if (sumWords >= 11 && sumWords < 16)
+            {
+                levelOfRisk = "ULTRA ALERT!";
+            }
+            else
+            {
+                levelOfRisk = "UNKNOWN RISK";
+            }
+
+            Console.WriteLine("The decrypted message is: " + message);
+            Console.WriteLine("The warning is: " + levelOfRisk);
+            Console.WriteLine("Sum of dangerous words: " + sumWords);
+        }
+
+
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine(  dangerousWords("jhtkvyb bomb kjhg bomb"));
+            Console.WriteLine(dangerousWords("jhtkvyb bomb kjhg bomb"));
         }
     }
 }
